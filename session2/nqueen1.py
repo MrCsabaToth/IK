7a0d8e8d5be2d0be51f3024174e158761d9ed189
MrCsabaToth <csaba.toth.us@gmail.com>
2019-11-21 23:32:03 -0800

    nqueen w/o backtrack

---
+++ session2/nqueen1.py
@@ -0,0 +1,37 @@
+def nqueen_helper(cols, rows, colinv, rowinv, n):
+    # Leaf node
+    if len(cols) == n:
+        return [",".join(["({}x{})".format(pos[0], pos[1]) for pos in zip(cols, rows)])]
+
+    result = []
+    # Internal node
+    for i in colinv[:]:
+        for j in rowinv[:]:
+            attack = False
+            for k in range(len(cols)):
+                dcol = abs(i - cols[k])
+                drow = abs(j - rows[k])
+                if dcol == drow:
+                    attack = True
+                    break
+
+            if not attack:
+                cols.append(i)
+                colinv.remove(i)
+                rows.append(j)
+                rowinv.remove(j)
+                result.extend(nqueen_helper(cols, rows, colinv, rowinv, n))
+                rowinv.append(j)
+                rows.pop()
+                colinv.append(i)
+                cols.pop()
+
+    return result
+
+
+def nqueen(n):
+    cols = []
+    rows = []
+    colinv = range(1, n + 1)
+    rowinv = range(1, n + 1)
+    return nqueen_helper(cols, rows, colinv, rowinv, n)
