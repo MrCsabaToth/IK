41a863dad22d18acc53340bb6c8f2adb48d3ebc6
MrCsabaToth <csaba.toth.us@gmail.com>
2019-11-23 14:24:16 -0800

    n queen practice

---
+++ practice2/nqueen1.py
@@ -0,0 +1,56 @@
+def find_all_arrangements(n):
+    pre_res = dict()
+    result = []
+
+    def add_result(cols, rows):
+        # print(cols, rows)
+        coords = list(zip(cols, rows))
+        coords.sort()
+        sol_str = "".join(["{}_{}_".format(c[0], c[1]) for c in coords])
+        if sol_str in pre_res:
+            return
+
+        pre_res[sol_str] = True
+        solution = []
+        for row in range(n):
+            solution.append("".join([("q" if (col, row) in coords else "-") for col in range(n)]))
+
+        result.append(solution)
+
+    def q_helper(cols, rows, scol, srow, level):
+        length = len(scol)
+        if length:
+            col = scol[length - 1]
+            row = srow[length - 1]
+            if any([(abs(scol[i] - col) == abs(srow[i] - row)) for i in range(length - 1)]):
+                return
+
+            if not len(cols):  # or not level:
+                # print(scol, srow, cols, rows)
+                add_result(scol, srow)
+                return
+
+        cols_copy = cols.copy()
+        rows_copy = rows.copy()
+
+        for c in cols_copy:
+            cols.remove(c)
+            scol.append(c)
+
+            for r in rows_copy:
+                rows.remove(r)
+                srow.append(r)
+                q_helper(cols, rows, scol, srow, level - 1)
+                srow.pop()
+                rows.append(r)
+
+            scol.pop()
+            cols.append(c)
+
+    cols0 = list(range(n))  # problem snippet columns part
+    rows0 = list(range(n))  # problem snippet rows part
+    scol0 = []  # solution columns part
+    srow0 = []  # solution rows part
+    q_helper(cols0, rows0, scol0, srow0, n)
+
+    return result
---
+++ session2/nqueen2.py
@@ -0,0 +1,62 @@
+def nqueen(n):
+    pre_res = dict()
+    result = []
+
+    def add_result(cols, rows):
+        # print(cols, rows)
+        coords = list(zip(cols, rows))
+        coords.sort()
+        sol_str = "".join(["{}_{}_".format(c[0], c[1]) for c in coords])
+        if sol_str in pre_res:
+            return
+
+        pre_res[sol_str] = True
+        solution = []
+        for row in range(n):
+            solution.append("".join([("q" if (col, row) in coords else "-") for col in range(n)]))
+
+        result.append(solution)
+
+    def nqueen_helper(cols, rows, colinv, rowinv, n):
+        length = len(cols)
+        if length:
+            col = cols[length - 1]
+            row = rows[length - 1]
+
+        # Leaf node
+        if length > 0:
+            if any([(abs(cols[i] - col) == abs(rows[i] - row)) for i in range(length - 1)]):
+                return
+
+            if length == n:
+                add_result(cols, rows)
+                return
+
+        result = set()
+        # Internal node
+        for i in colinv[:]:
+            for j in rowinv[:]:
+                attack = False
+                for k in range(len(cols)):
+                    dcol = abs(i - cols[k])
+                    drow = abs(j - rows[k])
+                    if dcol == drow:
+                        attack = True
+                        break
+
+                if not attack:
+                    cols.append(i)
+                    colinv.remove(i)
+                    rows.append(j)
+                    rowinv.remove(j)
+                    result.union(nqueen_helper(cols, rows, colinv, rowinv, n))
+                    rowinv.append(j)
+                    rows.pop()
+                    colinv.append(i)
+                    cols.pop()
+
+    cols = []
+    rows = []
+    colinv = range(1, n + 1)
+    rowinv = range(1, n + 1)
+    return nqueen_helper(cols, rows, colinv, rowinv, n)
