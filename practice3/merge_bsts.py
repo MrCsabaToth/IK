50af7f9f262bd1f3733081ddf43af5fbc0785cc8
MrCsabaToth <csaba.toth.us@gmail.com>
2019-12-07 15:23:21 -0800

    Merge BSTs practice passed

---
+++ practice3/merge_bsts.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, key): 
+        self.key = key  
+        self.left = None  
+        self.right = None
+
+def traverseInOrder(node):
+    if not node:
+        return []
+
+    traversal = traverseInOrder(node.left)
+    traversal.append(node.key)
+    traversal.extend(traverseInOrder(node.right))
+    return traversal
+
+
+def mergeSorted(a, b):
+    aptr = 0
+    alen = len(a)
+    bptr = 0
+    blen = len(b)
+    merged = [None] * (alen + blen)
+    i = 0
+    while aptr < alen or bptr < blen:
+        if bptr >= blen or aptr < alen and a[aptr] < b[bptr]:
+            merged[i] = a[aptr]
+            aptr += 1
+        else:
+            merged[i] = b[bptr]
+            bptr += 1
+        i += 1
+
+    return merged, (alen + blen)
+
+
+def buildBalancedBSTFromSorted(arr, start, end):
+    if start > end:
+        return None
+
+    # Overflow avoided (start + end) // 2 tought by Omkar (c)
+    mid = start + (end - start) // 2
+    node = Node(arr[mid])
+    node.left = buildBalancedBSTFromSorted(arr, start, mid - 1)
+    node.right = buildBalancedBSTFromSorted(arr, mid + 1, end)
+    return node
+
+
+# Complete this function and return root of the BST
+def mergeTwoBSTs(root1, root2):
+    # 1. Traverse root1 in-order (sorted outcome)
+    trav1 = traverseInOrder(root1)
+    # 2. Traverse root2 in-order (sorted outcome)
+    trav2 = traverseInOrder(root2)
+    # 3. Merge root1 + root2
+    merged, length = mergeSorted(trav1, trav2)
+    # 4. Build balanced BST from sorted array
+    return buildBalancedBSTFromSorted(merged, 0, length - 1)
