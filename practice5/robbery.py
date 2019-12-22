c7591586c098735f7495d6becd7723306d0993e3
MrCsabaToth <csaba.toth.us@gmail.com>
2019-12-21 23:39:28 -0800

    Correction of the robbery problem by the editorial

--- practice5/robbery.py
+++ practice5/robbery.py
@@ -2,9 +2,9 @@
 def maxStolenValue(values):
     length = len(values)
     dp = [0] * (length + 1)
+    dp[1] = values[0]
 
-    for i in range(1, length + 1):
-        subordinates = [dp[i] for i in range(i - 1)]
-        dp[i] = values[i - 1] + (max(subordinates) if subordinates else 0)
+    for i in range(2, length + 1):
+        dp[i] = max(dp[i - 1], dp[i - 2] + values[i - 1])
 
     return dp[length]
