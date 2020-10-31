c81134c6f4b1061b82c16a7c6b3a411a73dab14b
MrCsabaToth <csaba.toth.us@gmail.com>
2020-10-30 22:42:17 -0700

    Next palindrome problem at 15/44

---
+++ adhoc2/next_palindrome_wip_submission.py
@@ -0,0 +1,68 @@
+# Complete the function below.
+
+def next_palindrome(n):
+    digits = []
+    while n:
+        digits.insert(0, n % 10)
+        n = n // 10
+
+    if not digits:
+        digits = [0]
+
+    # Search for the first differring digit from the half back to the start
+    l = len(digits)
+    half = (l - 1) // 2
+    i = half
+    incd = False
+    while i >= 0:
+        if digits[i] == digits[l - 1 - i]:
+            i -= 1
+        else:
+            if digits[i] < digits[l - 1 - i]:
+                digits[i] += 1
+                incd = True
+
+            break
+
+    # We can mirror the rest of the beginning to the end
+    while i >= 0:
+        digits[l - 1 - i] = digits[i]
+        i -= 1
+
+    # Make sure the number will be bigger if there wasn't an increase so far
+    if not incd:
+        i = half
+        digits[i] += 1
+        if i != l - 1 - i:
+            digits[l - 1 - i] += 1
+
+    # Fix digits incremented from 9 to 10
+    i = half
+    while i >= 0:
+        carry = 0
+        if digits[i] >= 9:
+            digits[i] = 0
+            digits[l - 1 - i] = 0
+            carry = 1
+            j = i + 1
+            while j < half and carry:
+                digit = digits[j] + carry
+                if digit > 9:
+                    digit -= 10
+                else:
+                    carry = 0
+
+                digits[j] = digit
+                digits[l - 1 - j] = digit
+                j += 1
+
+        i -= 1
+
+    pal = 0
+    mult = 1
+    for digit in digits[::-1]:
+        pal += mult * digit
+        mult *= 10
+
+    return pal
+
