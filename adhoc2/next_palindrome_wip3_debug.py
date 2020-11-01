87e9bb36dcabc91d65d8fa0a838f2c8cc485a863
MrCsabaToth <csaba.toth.us@gmail.com>
2020-10-31 18:16:57 -0700

    Iteration on next palidrome, 30/44 score now

---
+++ adhoc2/next_palindrome_wip3_debug.py
@@ -0,0 +1,73 @@
+n = 2992 # 494
+
+digits = []
+while n:
+    digits.insert(0, n % 10)
+    n = n // 10
+    print("d1", digits)
+
+if not digits:
+    digits = [0]
+
+print("d2", digits)
+
+# Search for the first differring digit from the half back to the start
+l = len(digits)
+half = (l - 1) // 2
+i = half
+incd = False
+print("l", l, half, i, incd)
+while i >= 0:
+    if digits[i] == digits[l - 1 - i]:
+        print("i0", i)
+        i -= 1
+    else:
+        print("i3", i, l - 1 - i, digits[i], digits[l - 1 - i])
+        if digits[i] < digits[l - 1 - i]:
+            digits[i] += 1
+            incd = True
+
+        break
+
+    print("i1", i, digits)
+
+# We can mirror the rest of the beginning to the end
+while i >= 0:
+    if digits[l - 1 - i] < digits[i]:
+      incd = True
+    digits[l - 1 - i] = digits[i]
+    i -= 1
+    print("i2", i, digits)
+
+# Make sure the number will be bigger if there wasn't an increase so far
+if not incd:
+    i = half
+    digits[i] += 1
+    print("corr", i, digits)
+    if i != l - 1 - i:
+        digits[l - 1 - i] += 1
+        print("corr", i, digits)
+
+# Fix digits incremented from 9 to 10
+i = half
+carry = 0
+while i >= 0:
+    print("fix", i, carry, digits)
+
+    digit = digits[i] + carry
+    if carry or digit >= 9:
+        digits[i] = digit % 10
+        digits[l - 1 - i] = digit % 10
+
+        carry = digit // 10
+
+    i -= 1
+
+pal = 0
+mult = 1
+for digit in digits[::-1]:
+    pal += mult * digit
+    mult *= 10
+
+print(pal)
+
