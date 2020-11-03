e8765cba474ecd3c626b59e5412eee9baec6a72b
MrCsabaToth <csaba.toth.us@gmail.com>
2020-11-02 22:05:15 -0800

    38/39 Test passing now with the special handling of the b=1 case

---
+++ adhoc_test/constrained_division_wip3_submission.py
@@ -0,0 +1,33 @@
+def divide(a, b):
+    flip = False
+    if a < 0 and b >= 0 or a >= 0 and b < 0:
+        flip = True
+
+    a = abs(a)
+    b = abs(b)
+
+    if b == 1:
+        return a if not flip else -a
+
+    # Assemble b number base digit values
+    bs = [0] * 18
+    ms = [0] * 18
+    bs[0] = b
+    ms[0] = 1
+    for i in range(1, 18):
+        bs[i] = bs[i - 1] + bs[i - 1]
+        ms[i] = ms[i - 1] + ms[i - 1]
+        if bs[i] >= a:
+            break
+
+    q = 0
+    acc = 0
+    for j in range(i + 1)[::-1]:
+        if bs[j] + acc <= a:
+            acc += bs[j]
+            q += ms[j]
+
+    if flip:
+        q = -q
+
+    return q
