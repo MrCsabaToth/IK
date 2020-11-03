477fb9f3bf1ff86dd2e69ccbabfbf929f38447b6
MrCsabaToth <csaba.toth.us@gmail.com>
2020-11-02 19:57:45 -0800

    Trying to fix the last timing out test for the prime series test

---
+++ adhoc_test/primes_with_cache_wo_sieve.py
@@ -0,0 +1,23 @@
+cache = {}
+
+def is_prime(n):
+    if n in cache:
+        cache[n]
+
+    if n < 2:
+        return False
+    elif n < 4:
+        return True
+
+    sq = int(n**.5 + 0.5)
+    for i in range(2, sq + 1):
+        if n % i == 0:
+            cache[n] = False
+            return False
+
+    cache[n] = True
+    return True
+
+def detect_primes(a):
+    return "".join(["1" if is_prime(n) else "0" for n in a])
+
