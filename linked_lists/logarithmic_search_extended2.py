40ad411c61b74f6f90696f9d6069e73908f1b15a
MrCsabaToth <csaba.toth.us@gmail.com>
2021-01-02 22:20:34 -0800

    Max sliding window iterations, now all quick tests pass

---
+++ linked_lists/logarithmic_search_extended2.py
@@ -0,0 +1,113 @@
+def log_search(arr, val, find):
+    if not find and val > arr[-1]:
+        return len(arr)
+
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == val:
+            i = mid
+            while i >= 0 and arr[i] == val:
+                i -= 1
+
+            return i + 1
+        elif arr[mid] < val:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    if find:
+        return -1
+
+    return min(low, high)
+
+
+def max_in_sliding_window(arr, w):
+    win = sorted(arr[:w])
+    rlen = len(arr) - w + 1
+    ret = [None] * rlen
+    m = win[-1]
+    ret[0] = m
+    for i in range(rlen - 1):
+        out = arr[i]
+        pos1 = log_search(win, out, True)
+        print(out, pos1, win)
+        win = win[:pos1] + win[pos1 + 1:]
+        inp = arr[w + i]
+        pos2 = log_search(win, inp, False)
+        print(inp, pos2, win)
+        win.insert(pos2 + 1, inp)
+        m = win[-1]
+        ret[i + 1] = m
+        print(win, ret)
+
+    return ret
+
+
+import random
+import pytest
+
+
+#@pytest.mark.parametrize("arr,w,expected", [
+#    ([1, 3, -1, -3, 5, 3, 6, 7,], 3, [3, 3, 5, 5, 6, 7,])
+#])
+#def test_log_search_random(arr, w, expected):
+#    assert max_in_sliding_window(arr, w) == expected
+
+
+#def test_log_search_():
+#    arr = [-1, -3, 5]
+#    pos = log_search(arr, -1, True)
+#    assert pos == 0
+
+
+@pytest.mark.parametrize("i", range(100))
+def test_log_search_position(i):
+    rng = random.SystemRandom()
+    length = rng.randint(5, 20)
+    arr = [rng.randint(0, 1000) for j in range(length)]
+    arr.sort()
+    target = rng.randint(0, length - 1)
+    val = arr[target] - 1
+    target -= 1
+    while target >= 0 and arr[target] == val:
+        val -= 1
+        while target >= 0 and arr[target] == val:
+            target -= 1
+
+        target += 1
+
+    target = max(target, 0)
+
+    pos = log_search(arr, val, False)
+    print(arr, val, target, pos)
+    if pos >= 0:
+        assert arr[pos] < val
+    else:
+        assert arr[0] > val
+    if pos < length - 1:
+        assert arr[pos + 1] > val
+
+
+@pytest.mark.parametrize("i", range(3))
+def test_log_search_small_missing2(i):
+    rng = random.SystemRandom()
+    length = rng.randint(5, 20)
+    arr = [rng.randint(0, 1000) for j in range(length)]
+    arr.sort()
+    pos = log_search(arr, arr[0] - i - 1, False)
+    assert pos == -1
+
+
+@pytest.mark.parametrize("i", range(3))
+def test_log_search_big_missing2(i):
+    rng = random.SystemRandom()
+    length = rng.randint(5, 20)
+    arr = [rng.randint(0, 1000) for j in range(length)]
+    arr.sort()
+    pos = log_search(arr, arr[-1] + i + 1, False)
+    assert pos == length
+
+
+pytest.main()
---
+++ linked_lists/max_sliding_window_coderpad_wip1.py
@@ -0,0 +1,55 @@
+def log_search(arr, val, find):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == val:
+            i = mid
+            while i >= 0 and arr[i] == val:
+                i -= 1
+
+            return i + 1
+        elif arr[mid] < val:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    if find:
+        return -1
+
+    return max(min(low, high), 0)
+
+
+def max_in_sliding_window(arr, w):
+    win = sorted(arr[:w + 1])
+    rlen = len(arr) - w + 1
+    ret = [None] * rlen
+    m = win[-1]
+    ret[0] = m
+    for i in range(rlen - 1):
+        out = arr[i]
+        pos1 = log_search(win, out, True)
+        print(out, pos1, win)
+        win = win[:pos1] + win[pos1 + 1:]
+        inp = arr[w + i]
+        pos2 = log_search(win, inp, False)
+        print(inp, pos2, win)
+        win = win[:pos2] + [inp] + win[pos2 + 1:]
+        m = win[-1]
+        ret[i + 1] = m
+        print(win, ret)
+
+    return ret
+
+
+import pytest
+
+
+@pytest.mark.parametrize("arr,w,expected", [
+    ([1, 3, -1, -3, 5, 3, 6, 7,], 3, [3, 3, 5, 5, 6, 7,])
+])
+def test_log_search_random(arr, w, expected):
+    assert max_in_sliding_window(arr, w) == expected
+
+
+pytest.main()
