34a1a0ec09311de7b98577e94fc756afed390cf2
MrCsabaToth <csaba.toth.us@gmail.com>
2021-01-07 01:05:22 -0800

    4 / 24 quick test pass

---
+++ linked_lists/list_groupk_reverse_wip1.py
@@ -0,0 +1,32 @@
+'''
+For your reference:
+
+class LinkedListNode:
+    def __init__(self, node_value):
+        self.val = node_value
+        self.next = None
+'''
+def reverse_linked_list_in_groups_of_k(head, k):
+    p = head
+    prev = None
+    new_head = None
+    node = None
+    while p:
+        i = 0
+        q = []
+        while i < k and p:
+            q.append(p)
+            p = p.next
+
+        if not new_head:
+            new_head = q[-1]
+
+        for node in q[::-1]:
+            if prev:
+                prev.next = node
+
+            prev = node
+
+    node.next = None
+
+    return new_head
